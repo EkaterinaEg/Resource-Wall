65802eb98fe68b5d2a35a81d9dc54af702e88f26
Ekaterina Egorova <ekaterinan.egorova@gmail.com>
2023-11-22 12:36:45 -0500

    add html and CSS for single page

--- db/queries/resources.js
+++ db/queries/resources.js
@@ -70,7 +70,7 @@ const getResourcesbyUser = (user_id) => {
 const getResourcebyResourceId = (resource_id) => {
   return db
     .query(
-      `SELECT resources.*, ROUND(AVG(resource_ratings.rating),0) AS rating, STRING_AGG(DISTINCT categories.name, ', ') AS category, users.*, resource_comments.* FROM resources FULL OUTER JOIN users ON users.id = resources.user_id LEFT JOIN resource_categories ON resources.id = resource_categories.resource_id LEFT JOIN categories ON resource_categories.category_id = categories.id LEFT JOIN resource_ratings ON resources.id = resource_ratings.resource_id LEFT JOIN resource_comments ON resources.id = resource_comments.resource_id WHERE resources.id = ${resource_id} GROUP BY users.id, resources.id, resource_comments.id;`
+      `SELECT resources.*, ROUND(AVG(resource_ratings.rating),0) AS rating, STRING_AGG(DISTINCT categories.name, ', ') AS category, users.*, resource_comments.*, comment_creator.name AS comment_creator_name FROM resources FULL OUTER JOIN users ON users.id = resources.user_id LEFT JOIN resource_categories ON resources.id = resource_categories.resource_id LEFT JOIN categories ON resource_categories.category_id = categories.id LEFT JOIN resource_ratings ON resources.id = resource_ratings.resource_id LEFT JOIN resource_comments ON resources.id = resource_comments.resource_id LEFT JOIN users AS comment_creator ON resource_comments.user_id = comment_creator.id WHERE resources.id = ${resource_id} GROUP BY users.id, resources.id, resource_comments.id, comment_creator.name;`
     )
     .then((data) => {
       console.log(data.rows);
